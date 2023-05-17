1ab742083835e6df71df14dc84c1f5902ca06a8e
mohsin raza abid <125199140+MOHSIN-RAZA-ABID@users.noreply.github.com>
2023-05-17 19:43:40 +0500

    Add files via upload

---
+++ assignment13/script.js
@@ -0,0 +1,42 @@
+// we can find dom in html and javascript by this function
+
+//      a) accessing elements by class method
+
+//      b) acessing elements by id method
+
+//      c) accessing elements by tag name
+
+
+
+
+
+
+
+//method 1 for accessing html element by class
+
+
+let heading1 = documents.getElementsByClassName("heading");
+
+heading[0].style.color= "red";
+heading[1].style.border= "2px solid red";
+
+heading[1].style.backgroundcolor= "green";
+
+heading[1].style.padding= "4px 10px";
+
+
+
+//method 2 for accessing html element by id
+
+
+let heading2 = documents.getEoementsbyid("hadding1")
+
+
+hadding.innerhtml = "Ali"
+
+//   method 3 for accenssing elements by tag name
+
+
+let heading3 = documents.getElementsByClassName("heading")
+
+
